15382608caccad525147c0a4531468ae9f4db790
CHRISLAPTOP\choco <chocolat_chips1@yahoo.ca>
2024-11-25 14:52:05 -0700

    feat: Changed initial window size to better fit ClientView

--- WPFConversion/App.xaml.cs
+++ WPFConversion/App.xaml.cs
@@ -1,15 +1,28 @@
-﻿namespace WPFConversion
+﻿using WPFConversion.Views;
+
+namespace WPFConversion
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+            MainPage = new ClientView();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            Window window = base.CreateWindow(activationState);
+
+            //Width requires extra 20px to not reducce button size
+            int width = 820;
+            int height = 500;
+
+            window.Height = height;
+            window.Width = width;
+
+            return window;
+            //return new Window(new AppShell());
         }
     }
 }
