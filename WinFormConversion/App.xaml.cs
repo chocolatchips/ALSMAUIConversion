2eacc2eedd85f779b6a590dbbfbd1bdeec63eb2d
CHRISLAPTOP\choco <chocolat_chips1@yahoo.ca>
2024-11-26 20:26:49 -0700

    style: Changed style of WinFormsConversion to better reflect appearance of TestApp

--- WinFormConversion/App.xaml.cs
+++ WinFormConversion/App.xaml.cs
@@ -11,7 +11,9 @@ namespace WinFormConversion
         protected override Window CreateWindow(IActivationState? activationState)
         {
             Window window = new(new EmployeeView());
-
+            window.Title = "Employee Management";
+            window.Height = 400;
+            window.Width = 600;
             return window; 
         }
     }
